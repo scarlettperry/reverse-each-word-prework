d5270f19c3cde57081e6386f7f246b0c0d6da992
Scarlett Perry <si.perry15@gmail.com>
2018-07-01 22:19:51 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word (sentence1)
+  sentence2 = sentence1.split(" ") #changed to string
+  reverse = [] #new array to push reverse order
+  sentence2.collect do |word|
+    reverse.push(word.reverse)
+  end
+  reverse.join(" ")
+end
+
