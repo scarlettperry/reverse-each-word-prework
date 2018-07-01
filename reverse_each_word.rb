60bee4a7eaaac751cd19b0220dab84d9b96b0a2f
Scarlett Perry <si.perry15@gmail.com>
2018-07-01 22:01:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,8 @@ def reverse_each_word (sentence1)
   sentence2 = sentence1.split(" ") #changed to array
   reverse = [] #new array to push reverse order of sentence 2 
   sentence2.each do |word|
-    reverse.push(word)
-    
-    
-    
+    reverse.push(word.reverse)
+    reverse.join
   end
-  
-  
+  reverse
 end
